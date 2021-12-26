c0c81207d70d8d4c4bfdc084dbabc308cd8709bd
min <wyes91622@gmail.com>
2021-12-26 21:56:24 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import vueaxios from "vue-axios";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+Vue.use(axios, vueaxios);
 
 new Vue({
   vuetify,
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount("#app");
